2b9bad6829b427e7813885e0b5558e4979842f34
Selvendhiran Nagaraj <selvendhiran.nagaraj@gmail.com>
2022-04-16 20:50:06 -0500

    Cloud config microservices added

---
+++ cloudconfig/src/main/java/com/selva/retail/cloudconfig/CloudConfigMain.java
@@ -0,0 +1,11 @@
+package com.selva.retail.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigMain {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigMain.class,args);
+    }
+}
